835efa33997554a2cccb407828ef1e88bc414719
Martins Zeltins <martins.zeltins@gmail.com>
2024-12-10 12:19:25 +0200

    Initial commit

---
+++ server/api/sse.ts
@@ -0,0 +1,22 @@
+export default defineEventHandler(async (event) => {
+    setHeader(event, 'cache-control', 'no-cache')
+    setHeader(event, 'connection', 'keep-alive')
+
+    // This is what makes it an SSE (Server-Sent Events) endpoint
+    setHeader(event, 'content-type', 'text/event-stream')
+    
+    setResponseStatus(event, 200)
+
+    let counter = 0
+
+    const sendEvent = (data: any) => {
+        event.node.res.write(`id: ${++counter}\n`);
+        event.node.res.write(`data: ${JSON.stringify(data)}\n\n`);
+    }
+
+    sendEvent({ message: 'hello' })
+    setInterval(() => sendEvent({ message: 'ping' }), 1000)
+
+    // Keep the connection open
+    event._handled = true;
+})
